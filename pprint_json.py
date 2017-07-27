18fdf1e21aa959e53c8d597d7fed4e8d7f27d5e0
bdynamite <bdynamite@yandex.ru>
2017-07-27 10:44:44 +0300

    commit#4

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,13 @@
 import json
 
 
-def get_data(my_json_path):
+def read_json(my_json_path):
     try:
         with open(my_json_path, 'r') as file:
             return json.load(file)
     except FileNotFoundError:
         print("file not found!")
-        exit()
+        return None
 
 
 def pretty_print_json(json_data):
@@ -16,5 +16,6 @@ def pretty_print_json(json_data):
 
 if __name__ == '__main__':
     json_path = input('input data path: ')
-    json_data_text = get_data(json_path)
-    pretty_print_json(json_data_text)
+    json_text = read_json(json_path)
+    if json_text:
+        pretty_print_json(json_text)
